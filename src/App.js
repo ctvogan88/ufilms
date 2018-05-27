462457b6fa808f66569ef8d708cf2033eb2178e4
ctvogan88 <clayton.vogan@gmail.com>
2018-05-26 22:49:36 -0700

    hello world

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+import HelloDiv from "./components/HelloDiv";
+
+
+
+const App = () => <HelloDiv />;
+
+export default App;
---
+++ src/components/HelloDiv.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const HelloDiv = () => <h1>Hello World. Welcome to Ufilms!</h1>;
+
+export default HelloDiv;
