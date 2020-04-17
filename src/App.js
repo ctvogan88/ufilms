fedb6262c012d35e9d28f4c468226b229984913b
ctvogan88 <clayton.vogan@gmail.com>
2020-04-17 06:59:42 -0700

    under const commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './Uicon.svg';
 import './App.css';
+
 
 function App() {
   return (
@@ -8,15 +9,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Welcome! <code>Ufilms</code> is currently under construction.
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.youtube.com/channel/UCkCfeJCEIHPZamOa0lIlXew"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Watch videos here in the meantime..
         </a>
       </header>
     </div>
